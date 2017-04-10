be303826bfe0ea137a2a364128ec3d42c81b0c76
Sawyer3x <sawyer3xxx@gmail.com>
2017-04-10 17:45:32 +0800

    netwoker还没完成

---
+++ HalloPet/Network/DataModel/HPRequestBodyInfo.swift
@@ -0,0 +1,13 @@
+//
+//  HPRequestBodyInfo.swift
+//  HalloPet
+//
+//  Created by sawyer3x on 17/4/10.
+//  Copyright © 2017年 sawyer3x. All rights reserved.
+//
+
+import UIKit
+
+class HPRequestBodyInfo: NSObject {
+
+}
